6bf8e9f0751af350effa789b1ee89fc04a5c9b2a
Katalogo262f <bengalibiswas262f@gmail.com>
2024-09-10 12:26:57 +0530

    react-zoom-pan-pinch

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { Card } from "@/components/ui/card";
+import { AlignCenter, ZoomInIcon, ZoomOutIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ResetIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main>
+      <div className="w-screen h-screen overflow-hidden bg-gray-100">
+        {/* <main className="w-screen h-screen overflow-hidden">
+      <div className="w-screen h-screen overflow-hidden bg-gray-100"> */}
+        <TransformWrapper
+          initialScale={1}
+          // initialPositionX={screen.width / 2}
+          initialPositionY={screen.height / 4}
+          // centerOnInit={true}
+          minScale={0.5}
+          maxScale={5}
+          limitToBounds={false}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          {({
+            zoomIn,
+            zoomOut,
+            centerView,
+            resetTransform,
+            zoomToElement,
+            setTransform,
+            instance,
+          }) => (
+            <>
+              <div className="absolute top-2 right-2 z-10 space-x-2 bg-slate-200 rounded-full px-6 py-2">
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  onClick={() => zoomIn()}
+                >
+                  <ZoomInIcon className="h-4 w-4" />
+                </Button>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  onClick={() => zoomOut()}
+                >
+                  <ZoomOutIcon className="h-4 w-4" />
+                </Button>
+                {/* <Button
+                  variant="secondary"
+                  size="icon"
+                  onClick={() => resetTransform()}
+                >
+                  <ResetIcon className="h-4 w-4" />
+                </Button> */}
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  onClick={() => centerView(1)}
+                >
+                  <ResetIcon className="h-4 w-4" />
+                  {/* <AlignCenter className="h-4 w-4" /> */}
+                </Button>
+              </div>
+              <TransformComponent
+                wrapperClass="min-w-full min-h-full"
+                contentClass="min-w-full min-h-full"
+              >
+                <div className="w-full h-full flex justify-center">
+                  <Card className="text-black top-1/2 left-1/2 transform w-32 h-32 bg-red-500 flex items-center justify-center font-bold">
+                    Center
+                  </Card>
+                </div>
+              </TransformComponent>
+            </>
+          )}
+        </TransformWrapper>
+      </div>
+    </main>
   );
 }
